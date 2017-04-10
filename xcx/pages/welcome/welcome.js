80ea0a36920583152a493080d852b141207de3e1
youxingzhi <youxingzhi@qq.com>
2017-04-10 19:51:00 +0800

    初始化

--- xcx/pages/welcome/welcome.js
+++ xcx/pages/welcome/welcome.js
@@ -1,14 +1,12 @@
-//logs.js
-var util = require('../../utils/util.js')
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+    onTap: function (event) {
+        // wx.navigateTo({
+        //     url:"../posts/post"
+        // });
+        
+        wx.switchTab({
+            url: "../posts/post"
+        });
+        
+    }
 })
